322c8afd2101644962584e6b3b5df6a85aad6686
kevin <kai38547@gmail.com>
2024-10-30 17:56:52 -0400

    Add Storage location crud

---
+++ database/migrations/2024_10_30_214422_create_storage_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('storage_locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->foreignId('inventory_unit_id')->constrained('inventory_units')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('storage_locations');
+    }
+};
