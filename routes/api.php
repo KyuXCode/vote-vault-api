bcda6606a2de5f2402b7b8418529eac5bd866062
kevin <kai38547@gmail.com>
2024-11-05 11:36:29 -0500

    Add route for dashboard data

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\CertificationController;
 use App\Http\Controllers\Api\ComponentController;
 use App\Http\Controllers\Api\ContractController;
 use App\Http\Controllers\Api\CountyController;
+use App\Http\Controllers\Api\DashboardDataController;
 use App\Http\Controllers\Api\DispositionController;
 use App\Http\Controllers\Api\ExpenseController;
 use App\Http\Controllers\Api\InventoryUnitController;
@@ -33,3 +34,5 @@ Route::resource('inventory_units', InventoryUnitController::class);
 Route::resource('dispositions', DispositionController::class);
 
 Route::resource('storage_locations', StorageLocationController::class);
+
+Route::get('/api/dashboard_data', [DashboardDataController::class, 'getDashboardData']);
