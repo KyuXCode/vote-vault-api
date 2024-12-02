361c24d0814de21b30256e276e51805975e4dad6
Jose Rodriguez <jose.rodriguez@bsu.edu>
2024-12-02 09:29:48 -0500

    Update DispositionControllerTest.php

--- tests/Feature/DispositionControllerTest.php
+++ tests/Feature/DispositionControllerTest.php
@@ -4,12 +4,9 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Disposition;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class DispositionControllerTest extends TestCase
 {
-    use RefreshDatabase;
-
     /** @test */
     public function it_can_list_dispositions()
     {
