6afeedbc411e8628619ba6ca32bbc5fe302744fb
kevin <kai38547@gmail.com>
2025-01-16 12:32:21 -0500

    Refactor tests

--- app/Http/Controllers/Api/InventoryUnitController.php
+++ app/Http/Controllers/Api/InventoryUnitController.php
@@ -23,8 +23,8 @@ class InventoryUnitController extends Controller
         $data = $request->validate([
             'serial_number' => ['required', 'string', 'max:255'],
             'acquisition_date' => ['required', 'date'],
-            'condition' => ['required', new Enum(ConditionType::class)],
-            'usage' => ['required', new Enum(UsageType::class)],
+            InventoryUnit::condition => ['required', new Enum(ConditionType::class)],
+            InventoryUnit::usage => ['required', new Enum(UsageType::class)],
             'expense_id' => ['required', 'integer', 'exists:expenses,id'],
             'component_id' => ['required', 'integer', 'exists:components,id']
         ]);
@@ -44,8 +44,8 @@ class InventoryUnitController extends Controller
         $data = $request->validate([
             'serial_number' => ['sometimes', 'string', 'max:255'],
             'acquisition_date' => ['sometimes', 'date'],
-            'condition' => ['sometimes', new Enum(ConditionType::class)],
-            'usage' => ['sometimes', new Enum(UsageType::class)],
+            InventoryUnit::condition => ['sometimes', new Enum(ConditionType::class)],
+            InventoryUnit::usage => ['sometimes', new Enum(UsageType::class)],
             'expense_id' => ['sometimes', 'integer', 'exists:expenses,id'],
             'component_id' => ['sometimes', 'integer', 'exists:components,id']
         ]);
