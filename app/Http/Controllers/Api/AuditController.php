8efcbb409285deeaa572c125131d4c6f8a0f3553
Carson Batt <carson.batt@bsu.edu>
2025-02-03 21:13:12 -0500

    Fix PostgreSQL Laravel compatibility

--- app/Http/Controllers/Api/AuditController.php
+++ app/Http/Controllers/Api/AuditController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Str;
+use Random\RandomException;
 
 class AuditController extends Controller
 {
@@ -17,9 +18,10 @@ class AuditController extends Controller
         $seedNumber = request('seed_number', null);
 
         if (is_null($seedNumber)) {
-            $seedNumber = (int)(str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
-                str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
-                random_int(1, 99));
+            $seedNumber = random_int(1, PHP_INT_MAX);
+//            $seedNumber = (int)(str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
+//                str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
+//                random_int(1, 99));
         } else {
             $seedNumber = (int)$seedNumber;
         }
@@ -30,15 +32,17 @@ class AuditController extends Controller
         $validSystemBases = ['BMD', 'OPSCAN', 'DRE', 'VVPAT'];
 
         $results = DB::table('inventory_units as iu')
+            ->leftJoin('dispositions as d', 'iu.id', '=', 'd.inventory_unit_id')
             ->join('components as c', 'iu.component_id', '=', 'c.id')
             ->join('certifications as cert', 'c.certification_id', '=', 'cert.id')
             ->join('contracts as con', 'cert.id', '=', 'con.certification_id')
             ->join('expenses as exp', 'exp.contract_id', '=', 'con.id')
             ->join('counties as co', 'exp.county_id', '=', 'co.id')
-            ->leftJoin('dispositions as d', 'iu.id', '=', 'd.inventory_unit_id')
             ->whereNull('d.id')
             ->where('iu.usage', '!=', 'Inactive')
-            ->whereDate('con.end_date', '>=', now())
+            ->where('con.end_date', '>=', now())
+            ->whereIn('cert.system_type', ['VS', 'EPB'])
+
             ->select(
                 'iu.id as inventory_id',
                 'iu.serial_number',
@@ -48,7 +52,12 @@ class AuditController extends Controller
                 'c.type as component_type',
                 'cert.system_base',
                 'co.name as county_name',
-                DB::raw("ROW_NUMBER() OVER (PARTITION BY co.id, cert.system_base ORDER BY RANDOM() * $seedNumber) as row_num")
+                DB::raw("ROW_NUMBER() OVER (PARTITION BY co.id, cert.system_base ORDER BY RAND($seedNumber)) as row_num")
+
+            )
+            ->groupBy(
+                'iu.id', 'iu.serial_number', 'iu.condition', 'iu.usage',
+                'c.name', 'c.type', 'cert.system_base', 'co.name'
             )
             ->get()
             ->groupBy('county_name')
@@ -59,10 +68,16 @@ class AuditController extends Controller
                     $sampleSize = (int)ceil($systemBaseGroup->count() * 0.05);
                     $result = $result->merge($systemBaseGroup->take($sampleSize));
                 }
-                return $result->map(function ($item) {
-                    $item->temporary_guid = Str::uuid();
-                    return $item;
-                });
+                return $result;
+
+//                return $result->map(function ($item) {
+//                    $item->temporary_guid = Str::uuid();
+//                    return $item;
+//                });
+            })
+            ->flatten(1)
+            ->each(function ($item) {
+                $item->temporary_guid = (string) Str::uuid();
             });
 
         return response()->json([
@@ -79,27 +94,29 @@ class AuditController extends Controller
         $seedNumber = request('seed_number', null);
 
         if (is_null($seedNumber)) {
-            $seedNumber = (int)(str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
-                str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
-                random_int(1, 99));
+            $seedNumber = random_int(1, PHP_INT_MAX);
+//            $seedNumber = (int)(str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
+//                str_pad(random_int(1, 999999999), 9, '0', STR_PAD_LEFT) .
+//                random_int(1, 99));
         } else {
             $seedNumber = (int)$seedNumber;
         }
 
         // Set PHP's random seed
-        mt_srand($seedNumber);
+        //mt_srand($seedNumber);
 
         $samplePercentage = 0.0001; // 0.01%
 
         $results = DB::table('inventory_units as iu')
+            ->leftJoin('dispositions as d', 'iu.id', '=', 'd.inventory_unit_id')
             ->join('components as c', 'iu.component_id', '=', 'c.id')
             ->join('certifications as cert', 'c.certification_id', '=', 'cert.id')
             ->join('contracts as con', 'cert.id', '=', 'con.certification_id')
             ->join('expenses as exp', 'exp.contract_id', '=', 'con.id')
             ->join('counties as co', 'exp.county_id', '=', 'co.id')
-            ->leftJoin('dispositions as d', 'iu.id', '=', 'd.inventory_unit_id')
+
             ->whereNull('d.id')
-            ->whereDate('con.end_date', '>=', now())
+            ->where('con.end_date', '>=', now())
             ->whereIn('cert.system_type', ['VS', 'EPB'])
             ->select(
                 'iu.id as inventory_id',
@@ -107,20 +124,30 @@ class AuditController extends Controller
                 'iu.condition',
                 'iu.usage',
                 'c.name as component_name',
+                'c.type as component_type',
+                'cert.system_base',
                 'co.name as county_name',
                 DB::raw('COUNT(iu.id) OVER (PARTITION BY c.name) as total_count'),
-                DB::raw("ROW_NUMBER() OVER (PARTITION BY c.name ORDER BY RANDOM() * $seedNumber) as row_num")
+                DB::raw("ROW_NUMBER() OVER (PARTITION BY c.name ORDER BY RAND($seedNumber)) as row_num")
+            )
+
+
+            ->groupBy(
+                'iu.id', 'iu.serial_number', 'iu.condition', 'iu.usage',
+                'c.name', 'c.type', 'cert.system_base', 'co.name'
             )
             ->get()
             ->groupBy('component_name')
             ->map(function ($group) use ($samplePercentage) {
                 $totalCount = $group->first()->total_count;
-                $sampleSize = max(1, (int)ceil($totalCount * $samplePercentage));
+                $sampleSize = max(1, min(10, (int)ceil($totalCount * $samplePercentage)));
                 return $group->take($sampleSize);
             })
             ->flatten(1)
-            ->sortBy('county_name');
-
+            ->sortBy('county_name')
+            ->each(function ($item) {
+                $item->temporary_guid = (string) Str::uuid();
+            });
         return response()->json([
             'seed_number' => $seedNumber,
             'results' => $results->values(),
